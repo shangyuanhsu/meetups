31e7ba77be8c802c68a192530724b8ceb919bf63
shang <shang@cl.ncu.edu.tw>
2021-12-02 17:06:26 +0800

    new

--- src/views/CardContent.vue
+++ src/views/CardContent.vue
@@ -64,6 +64,108 @@
           Website: www.optimize-thrive-perform.com/<br />
           LinkedIn: https://www.linkedin.com/in/natasha-habert/<br />
         </p>
+        <div class="covid">
+          <h3>COVID-19 safety measures</h3>
+          <p>
+            The event host is instituting the above safety measures for this
+            event. Meetup is not responsible for ensuring, and will not
+            independently verify, that these precautions are followed.
+          </p>
+        </div>
+        <h3>Attendees (146)</h3>
+        <div class="attendees">
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>Ariel</p>
+          </div>
+          <div class="attendees_member">
+            <div>
+              <img :src="require('../assets/img/member_img/member.png')" />
+            </div>
+            <p>see all</p>
+          </div>
+        </div>
+        <h3>Comments</h3>
+        <div class="comments">
+          <button id="see_more_comments">see more comments</button>
+        </div>
+        <div>
+          <div id="show_comments">
+            <div class="comments_content">
+              <img :src="require('../assets/img/member_img/member.png')" />
+              <p>Natasha H.</p>
+              <p>
+                Hi all - Ariel and I are thrilled to give this presentation and
+                see you all soon! If you haven't already, please hit the
+                registration Zoom link under "Online Event" on the right side of
+                the page to register with your email address. Can't wait!
+              </p>
+              <p>8 days ago</p>
+              <div>like</div>
+              <div>report</div>
+            </div>
+            <div class="comments_content">
+              <img :src="require('../assets/img/member_img/member.png')" />
+              <p>Brennen F.</p>
+              <p>
+                Hi all! We are really looking forward to sharing this event with
+                you on Thursday. If you haven't already, please make sure you
+                sign up officially by registering using this link:
+                <br />
+                Thanks so much!
+              </p>
+              <p>8 days ago</p>
+              <div>like</div>
+              <div>report</div>
+            </div>
+            <div class="comments_content">
+              <img :src="require('../assets/img/member_img/member.png')" />
+              <p>Anna.</p>
+              <p>I’m very much looking forward to this event!</p>
+              <p>8 days ago</p>
+              <div>like</div>
+              <div>report</div>
+            </div>
+          </div>
+        </div>
       </section>
       <section>
         <div class="other_info_zone">
@@ -145,6 +247,7 @@ main {
   justify-content: space-between;
   margin: 0px auto;
 }
+
 #show_content {
   word-break: break-all;
   line-height: 1.5em;
@@ -159,12 +262,97 @@ main {
   top: 20px;
 }
 .other_info {
-  width: 100%;
-  /* height: 150px; */
+  width: 80%;
   background: white;
   border-radius: 24px;
-  margin: 20px 0 20px 50px;
+  margin: 20px 0 20px auto;
   padding: 20px 30px;
   line-height: 1.5em;
+}
+.other_info span {
+  color: rgb(177, 179, 197);
+  margin-right: 5px;
+  cursor: pointer;
+}
+.other_info span:hover {
+  color: rgb(34, 34, 34);
+}
+.covid {
+  margin: 40px 0;
+  background: white;
+  border-radius: 24px;
+  padding: 30px;
+  line-height: 1.5em;
+}
+.covid h3 {
+  margin: 0 0 10px 0;
+}
+.covid p {
+  color: gray;
+}
+.attendees {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  justify-items: center;
+}
+.attendees_member {
+  background: rgba(255, 255, 255, 0.76);
+  width: 80%;
+  margin: 10px 0;
+  padding: 30px;
+  border-radius: 12px;
+  /* overflow: hidden; */
+}
+.attendees_member:last-child {
+  background: rgba(255, 255, 255, 0.301);
+}
+.attendees_member p {
+  text-align: center;
+}
+.attendees_member div {
+  width: 70px;
+  height: 70px;
+  border-radius: 70%;
+  overflow: hidden;
+  margin: 10px auto;
+}
+.attendees_member div img {
+  width: 100%;
+}
+#see_more_comments {
+  background: none;
+  display: block;
+  width: 100%;
+  padding: 10px;
+  text-align: center;
+  font-size: 20px;
+  outline: none;
+  cursor: pointer;
+  padding: 10px;
+  border-radius: 4px;
+  color: #77002e;
+  border: 1px solid #77002e;
+}
+.comments_content {
+  margin: 20px 0;
+  background: rgba(255, 255, 255, 0.76);
+  border-radius: 12px;
+  padding: 30px;
+  line-height: 1.5em;
+}
+.comments_content > p:first-of-type {
+  font-weight: bold;
+}
+.comments_content > p:last-of-type {
+  margin: 10px 0;
+  color: rgb(196, 196, 196);
+}
+.comments_content div {
+  display: inline;
+  padding: 5px;
+  color: #77002e;
+  border: 1px solid #77002e;
+  border-radius: 4px;
+  margin-right: 5px;
 }
 </style>
